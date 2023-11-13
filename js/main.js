8c62ee1e6b8d2794ce2ab6d1ee20ce5372dcaaaf
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-11-13 16:51:18 -0400

    .

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ var constraints = { video: { facingMode: camMode }, audio:false };
 const cameraView = document.querySelector("#camera--view"),
  cameraOutput = document.querySelector("#camera--output"),
  cameraSensor = document.querySelector("#camera--sensor"),
- cameraTrigger = document.querySelector("#camera--trigger");
+ cameraTrigger = document.querySelector("#camera--trigger"),
  frenteTraz = document.querySelector("#frente--Traz");
 
 function cameraStart(){
